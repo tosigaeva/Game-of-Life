d7f6d6f877b2eeda819bbcc8f9070e097e0c96e6
ivansigaev <asd@asd.ru>
2022-01-20 22:50:00 +0100

    fix test refactor logic

--- src/init.js
+++ src/init.js
@@ -1,40 +1,42 @@
+let amountOfActive = 0;
+
 function getCell(active = true) {
     return {
         active
     };
 }
 
-function calcStatus(k, n, i) {
-    const status = k > 0 && (n - i <= k || Math.random() >= 0.5);
-    if (status) {
-        k--;
-    }
-    return {k, status};
+function calcStatus(amountToSetActive, allItems, currentItem) {
+    let delta = amountToSetActive - amountOfActive;
+    const status = delta > 0 && (allItems - currentItem <= delta || Math.random() >= 0.5);
+    amountOfActive = status ? amountOfActive + 1 : amountOfActive;
+
+    return status;
 }
 
-function multiArray(n, m) {
+function multiArray(n, m, k) {
     let a = [];
     for (let i = 0; i < n; ++i) {
         a[i] = [];
         for (let j = 0; j < m; ++j) {
-            a[i][j] = getCell();
+            a[i][j] = getCell(calcStatus(k, n * m, i * m + j))
         }
     }
+
     return a;
 }
 
 function singleArray(n, k) {
     let a = [];
     for (let i = 0; i < n; ++i) {
-        const __ret = calcStatus(k, n, i);
-        k = __ret.k;
-        a[i] = getCell(__ret.status);
+        a[i] = getCell(calcStatus(k, n, i));
     }
     return a;
 }
 
-function init(n, m, k) {
-    return m === 0 ? singleArray(n, k) : multiArray(n, m);
+function init(n, m, k = 0) {
+    amountOfActive = 0;
+    return m === 0 ? singleArray(n, k) : multiArray(n, m, k);
 }
 
 module.exports = init
--- src/init.test.js
+++ src/init.test.js
@@ -1,46 +1,80 @@
 const init = require('./init');
 
-test('0x0 0 active is []', () => {
-    expect(init(0, 0)).toStrictEqual([]);
+test('0x0 and 0 active', () => {
+    expect(init(0, 0, 0)).toStrictEqual([]);
 });
 
-test('1x0 0 activ is [1]', () => {
+test('0x0 and 3 active', () => {
+    expect(init(0, 0, 3)).toStrictEqual([]);
+});
+
+test('1x0 and 0 active', () => {
     expect(init(1, 0, 0)).toStrictEqual([{active: false}]);
 });
 
-test('1x0 1 active is [1]', () => {
+test('1x0 and 1 active', () => {
     expect(init(1, 0, 1)).toStrictEqual([{active: true}]);
 });
 
-test('0xm is []', () => {
+test('1x0 and 3 active', () => {
+    expect(init(1, 0, 3)).toStrictEqual([{active: true}]);
+});
+
+test('0xm and k active', () => {
     expect(init(0, 3, 10)).toStrictEqual([]);
 });
 
-test('3x0 is 0 active [{active: false}, {active: false}, {active: false}]', () => {
+test('3x0 and no active', () => {
     expect(init(3, 0, 0)).toStrictEqual([{active: false}, {active: false}, {active: false}]);
 });
 
-test('3x0 is 3 active [{active: true}, {active: true}, {active: true}]', () => {
+test('3x0 and all active', () => {
     expect(init(3, 0, 3)).toStrictEqual([{active: true}, {active: true}, {active: true}]);
 });
 
-test('3x0 is 2 active', () => {
-    expect(init(3, 0, 2).reduce((a, b) => b.active ? ++a : a, 0), 2);
+test('3x0 and 2 active', () => {
+    expect(init(3, 0, 2).reduce((a, b) => b.active ? ++a : a, 0)).toBe(2);
 });
 
-test('3x0 is [1, 1, 1]', () => {
-    expect(init(3, 0)).toStrictEqual([1, 1, 1]);
+test('3x0 has length 3', () => {
+    expect(init(3, 0).length).toBe(3);
 });
 
-test('1x1 is [[1]]', () => {
-    expect(init(1, 1)).toStrictEqual([[1]]);
+test('array: 1x1 and no active', () => {
+    const table = init(1, 1, 0);
+    expect(table).toStrictEqual([[{active: false}]]);
+})
+
+test('array: 1x1 and all active', () => {
+    expect(init(1, 1, 1)).toStrictEqual([[{active: true}]]);
+})
+
+test('array: 1x1 has length 1', () => {
+    expect(init(1, 1).length).toBe(1);
 });
 
-
-test('5x3 is [[1, 1, 1], [1, 1, 1], [1, 1, 1]...]', () => {
-    expect(init(5, 3)).toStrictEqual([[1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1], [1, 1, 1]]);
+test('array: 1x1 has sub-array with length 1', () => {
+    expect(init(1, 1)[0].length).toBe(1);
 });
 
-test('3x5 is [[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], ...]', () => {
-    expect(init(3, 5)).toStrictEqual([[1, 1, 1, 1, 1], [1, 1, 1, 1, 1], [1, 1, 1, 1, 1]]);
+test('array: 1x1 has total items 1', () => {
+    expect(init(1, 1).flat().length).toBe(1);
 });
+
+test('array: 3x5 and no active', () => {
+    expect(init(3, 5, 0)).toStrictEqual([[{active: false}, {active: false}, {active: false}, {active: false}, {active: false}],
+        [{active: false}, {active: false}, {active: false}, {active: false}, {active: false}],
+        [{active: false}, {active: false}, {active: false}, {active: false}, {active: false}]]);
+})
+
+test('array: 3x5 and all active', () => {
+    expect(init(3, 5, 15)).toStrictEqual([[{active: true}, {active: true}, {active: true}, {active: true}, {active: true}],
+    [{active: true}, {active: true}, {active: true}, {active: true}, {active: true}],
+    [{active: true}, {active: true}, {active: true}, {active: true}, {active: true}]]);
+})
+
+test('array: 3x5 and 7 active', () => {
+    const table = init(3, 5, 7);
+    expect(table.flat().reduce((a, b) => b.active ? ++a : a, 0)).toBe(7);
+    expect(table.filter((item) => item.length === 5).length).toBe(3);
+})

0be6b68a6e0487d3ce86861d1985beac7ea76fb4
ivansigaev <asd@asd.ru>
2022-02-10 16:03:35 +0100

    fix old code

--- src/generateBoard.js
+++ src/generateBoard.js
@@ -9,14 +9,37 @@ function generateBoard(currentBoard) {
         for (let j = 0; j < currentBoard[i].length; j++) {
             let neighbors = 0;
 
-            if(isNeighborFromTopFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromTopRigthFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromRigthFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromBottomRigthFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromBottomFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromBottomLeftFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromLeftFound(currentBoard, i, j)) {neighbors++;}
-            if(isNeighborFromTopLeftFound(currentBoard, i, j)) {neighbors++;}
+            if(isNeighborFromTopFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromTopRigthFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromRigthFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromBottomRigthFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromBottomFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromBottomLeftFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromLeftFound(currentBoard, i, j)) {
+                neighbors++;
+            }
+
+            if(isNeighborFromTopLeftFound(currentBoard, i, j)) {
+                neighbors++;
+            }
 
             newBoard[i][j] = newStatus(currentBoard[i][j], neighbors);
         }
@@ -26,7 +49,7 @@ function generateBoard(currentBoard) {
 }
 
 function isNeighborFromTopFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === 0 ? board.length -1 : rowIndex - 1;
+    rowIndex = rowIndex === 0 ? board.length - 1 : rowIndex - 1;
 
     return board[rowIndex][columnIndex];
 }
@@ -52,13 +75,13 @@ function isNeighborFromBottomRigthFound(board, rowIndex, columnIndex) {
 }
 
 function isNeighborFromBottomFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === board.length -1 ? 0 : rowIndex + 1;
+    rowIndex = rowIndex === board.length - 1 ? 0 : rowIndex + 1;
 
     return board[rowIndex][columnIndex];
 }
 
 function isNeighborFromBottomLeftFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === board.length -1 ? 0 : rowIndex + 1;
+    rowIndex = rowIndex === board.length - 1 ? 0 : rowIndex + 1;
     columnIndex = columnIndex === 0 ? board[rowIndex].length - 1 : columnIndex - 1;
 
     return board[rowIndex][columnIndex];
@@ -71,7 +94,7 @@ function isNeighborFromLeftFound(board, rowIndex, columnIndex) {
 }
 
 function isNeighborFromTopLeftFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === 0 ? board.length -1 : rowIndex - 1;
+    rowIndex = rowIndex === 0 ? board.length - 1 : rowIndex - 1;
     columnIndex = columnIndex === 0 ? board[rowIndex].length - 1 : columnIndex - 1;
 
     return board[rowIndex][columnIndex];

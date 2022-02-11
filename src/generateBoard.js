4781ce169b674249a1daced8229aa2dd496a7e63
ivansigaev <asd@asd.ru>
2022-02-11 13:40:46 +0100

    fix old code

--- src/generateBoard.js
+++ src/generateBoard.js
@@ -1,103 +1,54 @@
 const newStatus = require('./newStatus');
 
-function generateBoard(currentBoard) {
+function generateBoard(board) {
     let newBoard = [];
 
-    for (let i = 0; i < currentBoard.length; i++) {
+    for (let i = 0; i < board.length; i++) {
         newBoard[i] = [];
+        let verticalShift = getCellIndexes(board, i);
 
-        for (let j = 0; j < currentBoard[i].length; j++) {
-            let neighbors = 0;
+        for (let j = 0; j < board[i].length; j++) {
+            let horizontalShift = getCellIndexes(board[i], j);
+            let neighbors = getNeighbors(board, verticalShift, horizontalShift);
+            let numberOfLivingNeighbors = Object.values(neighbors).filter(item => item === true).length;
 
-            if(isNeighborFromTopFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromTopRigthFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromRigthFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromBottomRigthFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromBottomFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromBottomLeftFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromLeftFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            if(isNeighborFromTopLeftFound(currentBoard, i, j)) {
-                neighbors++;
-            }
-
-            newBoard[i][j] = newStatus(currentBoard[i][j], neighbors);
+            newBoard[i][j] = newStatus(board[i][j], numberOfLivingNeighbors);
         }
     }
 
     return newBoard;
 }
 
-function isNeighborFromTopFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === 0 ? board.length - 1 : rowIndex - 1;
+function getCellIndexes(board, currentIndex) {
+    let prevIndex = currentIndex - 1;
+    let nextIndex = currentIndex + 1;
 
-    return board[rowIndex][columnIndex];
+    if (currentIndex === 0) {
+        prevIndex = board.length - 1;
+    }
+
+    if (currentIndex === board.length - 1) {
+        nextIndex = 0;
+    }
+
+    return {
+        prevIndex,
+        currentIndex,
+        nextIndex
+    };
 }
 
-function isNeighborFromTopRigthFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === 0 ? board.length -1 : rowIndex - 1;
-    columnIndex = columnIndex === board[rowIndex].length - 1 ? 0 : columnIndex + 1;
-
-    return board[rowIndex][columnIndex];
-}
-
-function isNeighborFromRigthFound(board, rowIndex, columnIndex) {
-    columnIndex = columnIndex === board[rowIndex].length - 1 ? 0 : columnIndex + 1;
-
-    return board[rowIndex][columnIndex];
-}
-
-function isNeighborFromBottomRigthFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === board.length - 1 ? 0 : rowIndex + 1;
-    columnIndex = columnIndex === board[rowIndex].length - 1 ? 0 : columnIndex + 1;
-
-    return board[rowIndex][columnIndex];
-}
-
-function isNeighborFromBottomFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === board.length - 1 ? 0 : rowIndex + 1;
-
-    return board[rowIndex][columnIndex];
-}
-
-function isNeighborFromBottomLeftFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === board.length - 1 ? 0 : rowIndex + 1;
-    columnIndex = columnIndex === 0 ? board[rowIndex].length - 1 : columnIndex - 1;
-
-    return board[rowIndex][columnIndex];
-}
-
-function isNeighborFromLeftFound(board, rowIndex, columnIndex) {
-    columnIndex = columnIndex === 0 ? board[rowIndex].length - 1 : columnIndex - 1;
-
-    return board[rowIndex][columnIndex];
-}
-
-function isNeighborFromTopLeftFound(board, rowIndex, columnIndex) {
-    rowIndex = rowIndex === 0 ? board.length - 1 : rowIndex - 1;
-    columnIndex = columnIndex === 0 ? board[rowIndex].length - 1 : columnIndex - 1;
-
-    return board[rowIndex][columnIndex];
+function getNeighbors(board, verticalIndexes, horizontalIndexes) {
+    return {
+        neighborTop: board[verticalIndexes.prevIndex][horizontalIndexes.currentIndex],
+        neighborTopRigth: board[verticalIndexes.prevIndex][horizontalIndexes.nextIndex],
+        neighborRigth: board[verticalIndexes.currentIndex][horizontalIndexes.nextIndex],
+        neighborBottomRigth: board[verticalIndexes.nextIndex][horizontalIndexes.nextIndex],
+        neighborBottom: board[verticalIndexes.nextIndex][horizontalIndexes.currentIndex],
+        neighborBottomLeft: board[verticalIndexes.nextIndex][horizontalIndexes.prevIndex],
+        neighborLeft: board[verticalIndexes.currentIndex][horizontalIndexes.prevIndex],
+        neighborTopLeft: board[verticalIndexes.prevIndex][horizontalIndexes.prevIndex]
+    }
 }
 
 module.exports = generateBoard
